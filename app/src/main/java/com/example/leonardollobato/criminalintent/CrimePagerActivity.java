ec25d8949e3d51045cb30c6a3842744a0a503d0b
Leonardo Lobato <leonardo.lobato@gmail.com>
2016-06-08 15:56:27 -0300

    toolbar/actionbar

--- app/src/main/java/com/example/leonardollobato/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/leonardollobato/criminalintent/CrimePagerActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.ViewGroup;
 
@@ -17,7 +18,7 @@ import java.util.UUID;
 /**
  * Created by leonardollobato on 6/1/16.
  */
-public class CrimePagerActivity extends FragmentActivity {
+public class CrimePagerActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
